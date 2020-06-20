707703b843dd9a4889fb614fdf4483afb111c78d
AllanKovac <allankfrasco@gmail.com>
2020-06-20 19:10:43 -0300

    Criando evento de clic nos botoes

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -9,7 +9,7 @@ class CalcController{
         this._displayCalc = "0";
         this._currentDate;
         this.initialize();
-
+        this.initButtonsEvents();
     }
 
     initialize(){
@@ -22,6 +22,12 @@ class CalcController{
 
     initButtonsEvents(){
         let buttons = document.querySelectorAll("#buttons > g, #parts > g");
+
+        buttons.forEach((btn, index) => {
+            btn.addEventListener('click', e=>{
+                console.log(btn.className.baseVal.replace('btn-', ''));
+            });
+        });
     }
 
     setDisplayDateTime(){
